0b9a120cb351f1cb71c1fdb20937fc4596b15424
Qinphp <qinphp@qq.com>
2018-12-21 06:39:42 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -16,20 +21,21 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, ImageUploadHandler $uploader,User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
-        if($request->avatar){
+        if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
-            if($result){
+            if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
-
 
         $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
